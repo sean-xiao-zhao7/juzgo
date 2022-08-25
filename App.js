3d206cea2cafd918231c38cfb57c3ed68f90a302
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-08-25 12:49:25 -0400

    Add login screen

--- App.js
+++ App.js
@@ -1,3 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// screens
+import LoginScreen from "./src/screens/auth/LoginScreen";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-    return <></>;
+    return (
+        <>
+            <StatusBar style="auto" />
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="LoginScreen" component={LoginScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
 }
