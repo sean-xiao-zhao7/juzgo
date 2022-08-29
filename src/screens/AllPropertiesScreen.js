6e6ae2d06ac769e56016c5703360b4004f50a46b
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-08-29 14:19:48 -0400

    Update home screen

--- src/screens/AllPropertiesScreen.js
+++ src/screens/AllPropertiesScreen.js
@@ -5,7 +5,12 @@ import TextRegular from "../components/texts/TextRegular";
 const AllPropertiesScreen = (props) => {
     return (
         <ScreenContainer>
-            <TextRegular>Welcome Landlord!</TextRegular>
+            <TextRegular style={{ marginBottom: 20 }}>
+                Welcome, Landlord!
+            </TextRegular>
+            <TextRegular style={{ marginBottom: 30, width: 300 }}>
+                This the home page that shows all your properties.
+            </TextRegular>
         </ScreenContainer>
     );
 };
