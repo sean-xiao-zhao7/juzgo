3b8b61f6e47de47d725f22b4c9b57c202b6cbd79
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-09-15 15:08:30 -0400

    Add inquiries title

--- src/screens/inquiries/InquiriesScreen.js
+++ src/screens/inquiries/InquiriesScreen.js
@@ -2,24 +2,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState, useLayoutEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { Pressable } from "react-native";
 
 // redux
 
 // comps
 import ScreenContainer from "../../components/containers/ScreenContainer";
-import TextRegular from "../../components/texts/TextRegular";
-import { Pressable } from "react-native";
 
 const InquiriesScreen = (props) => {
     const dispatch = useDispatch();
 
     useLayoutEffect(() => {
         props.navigation.setOptions({
-            title: "",
+            title: "Inquiries Log",
             headerRight: () => {
                 return (
                     <Pressable onPress={closeHandler}>
-                        <FontAwesomeIcon icon={faXmark} size={40} />
+                        <FontAwesomeIcon icon={faXmark} size={30} />
                     </Pressable>
                 );
             },
