80af8c0e5e92f6c26273e92e5b802c399ba323d9
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-12-16 12:55:16 -0500

    Fix add inquiry textbox hidden

--- src/screens/inquiries/AddInquiryScreen.js
+++ src/screens/inquiries/AddInquiryScreen.js
@@ -2,7 +2,7 @@ import { useLayoutEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
 // comps
-import ScreenContainer from "../../components/containers/ScreenContainer";
+import ScreenScrollContainer from "../../components/containers/ScreenScrollContainer";
 import CustomTextInput from "../../components/inputs/CustomTextInput";
 import Button1 from "../../components/buttons/Button1";
 
@@ -12,6 +12,7 @@ import { addInquiryAPI } from "../../store/slices/inquirySlice";
 // form
 import { incompleteErrorAlert } from "../../components/forms/helpers/alert";
 import { emptyVerify } from "../../components/forms/helpers/verifyForm";
+import { KeyboardAvoidingView } from "react-native";
 
 const AddInquiryScreen = (props) => {
     const dispatch = useDispatch();
@@ -31,30 +32,34 @@ const AddInquiryScreen = (props) => {
     };
 
     return (
-        <ScreenContainer>
-            <CustomTextInput
-                placeholder={"Reason of Inquiry"}
-                onChangeText={setTitle}
-                value={title}
-            />
-            <CustomTextInput
-                placeholder={"Please provide the detail of the inquiry."}
-                onChangeText={setDescription}
-                value={description}
-                multiline={true}
-                style={{ height: 100 }}
-            />
-            <Button1
-                text={"Start inquiry"}
-                onPress={() => {
-                    if (emptyVerify([title, description])) {
-                        addInquiryHandler();
-                    } else {
-                        incompleteErrorAlert();
-                    }
-                }}
-            />
-        </ScreenContainer>
+        <ScreenScrollContainer>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+            >
+                <CustomTextInput
+                    placeholder={"Reason of Inquiry"}
+                    onChangeText={setTitle}
+                    value={title}
+                />
+                <CustomTextInput
+                    placeholder={"Please provide the detail of the inquiry."}
+                    onChangeText={setDescription}
+                    value={description}
+                    multiline={true}
+                    style={{ height: 100 }}
+                />
+                <Button1
+                    text={"Start inquiry"}
+                    onPress={() => {
+                        if (emptyVerify([title, description])) {
+                            addInquiryHandler();
+                        } else {
+                            incompleteErrorAlert();
+                        }
+                    }}
+                />
+            </KeyboardAvoidingView>
+        </ScreenScrollContainer>
     );
 };
 
